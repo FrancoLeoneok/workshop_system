fe56137b6711b9330f7ee5ef7abd23a3e64d32d5
FrancoLeoneok <francolopez381@gmail.com>
2025-12-11 12:43:24 -0300

    Add Dispositivo class with string representation
    Fix import statement and add Dispositivo class.

--- clase.py
+++ clase.py
@@ -1,1 +1,13 @@
+import funtion 
+import datetime
+
+class Dispositivo:
+  def __init__(self, tipo: str, marca, modelo):
+    self.tipo = tipo
+    self.marca = marca
+    self.modelo = modelo
+
+
+  def __str__(self):
+    return f"Dispositivo(tipo={self.tipo)"
 
